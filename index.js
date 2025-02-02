e442f0497f9f8cc47d46d10b3171b02d23122090
Patrick Radu Stanca Munteanu <120978917+PatrickRadu@users.noreply.github.com>
2025-02-02 13:22:39 +0200

    Middlewere errors

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ let notes = [
     }
   ]
 
+const errorHandler=(error,request,response,next)=>{
+    console.log(error.message)
+    if (error.name==='CastError'){
+        return response.status(400).send({error:'malformatted id'})
+    }
+    next(error)
+}
 const requestLogger=(request,response,next)=>{
     console.log('Method:',request.method)
     console.log('Path:',request.path)
@@ -48,10 +55,17 @@ app.get('/api/notes',(request,response)=>{
     })
 })
 
-app.get('/api/notes/:id',(request,response)=>{
+app.get('/api/notes/:id',(request,response,next)=>{
    const id=request.params.id
    Note.findById(id).then(note=>{
+         if(note){
          response.json(note)
+         }
+         else{
+            response.status(404).end()
+         }
+    }).catch(error=>{
+        next(error)
     })
 })
 
@@ -59,7 +73,8 @@ app.delete("/api/notes/:id",(request,response)=>{
     const id=request.params.id
     Note.findByIdAndRemove(id).then(result=>{
         response.status(204).end()
-    })
+    }).catch(error=>next(error)
+    )
 })
 
 app.post('/api/notes',(request,response)=>{
@@ -76,12 +91,28 @@ app.post('/api/notes',(request,response)=>{
     })
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+  
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
+  })
+
 
 const unknownEndpoint=(request,response)=>{
     response.status(404).send({error:'unknown endpoint'})
 }
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
