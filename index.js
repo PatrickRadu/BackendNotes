3310efe6e862c2c3f0a27fa314cf8b6f4ce6e9fd
Patrick Radu Stanca Munteanu <120978917+PatrickRadu@users.noreply.github.com>
2025-02-02 13:29:19 +0200

    More validations

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ const errorHandler=(error,request,response,next)=>{
     if (error.name==='CastError'){
         return response.status(400).send({error:'malformatted id'})
     }
+    else if(error.name==='ValidationError'){
+        return response.status(400).json({error:error.message})
+    }
     next(error)
 }
 const requestLogger=(request,response,next)=>{
@@ -77,7 +80,7 @@ app.delete("/api/notes/:id",(request,response)=>{
     )
 })
 
-app.post('/api/notes',(request,response)=>{
+app.post('/api/notes',(request,response,next)=>{
     const body=request.body
     if (body.content===undefined){
         return response.status(400).json({error:'content missing'})
@@ -89,6 +92,7 @@ app.post('/api/notes',(request,response)=>{
     note.save().then(savedNote=>{
         response.json(savedNote)
     })
+    .catch(error=>next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -99,7 +103,7 @@ app.put('/api/notes/:id', (request, response, next) => {
       important: body.important,
     }
   
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(request.params.id,{content,important}, { new: true,runValidators:true,context:'query' })
       .then(updatedNote => {
         response.json(updatedNote)
       })
